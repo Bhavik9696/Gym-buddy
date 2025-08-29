d83c00a6bf5eb9d714390b5966db6ab2ed4a5c37
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-08-29 21:47:20 +0530

    correct post controler

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -5,9 +5,9 @@ const createWorkout=async(req,res)=>{
   const {title,load,reps}=req.body;
 
   try{
-    const workout = await Workout.create({ title, load, reps });
+    const createworkout = await workout.create({ title, load, reps });
 
-    res.status(200).json(workout); }catch(error){
+    res.status(200).json(createworkout); }catch(error){
   res.status(400).json({error:error.message})
 
   }
